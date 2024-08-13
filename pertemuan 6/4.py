21a2e9d4e7a211667ac8a813eab44d543de66469
fathan432 <alfatharfathan@gmail.com>
2024-08-13 09:59:22 +0700

    Add files via upload

---
+++ pertemuan 6/4.py
@@ -0,0 +1,13 @@
+x = 10
+y = 7
+z = 5
+if x>5:
+    print("x is greater than 5")   
+elif x == 5:
+    print("x is less than 5") 
+elif:
+    print("x is less than 5")
+elif z>x and z>y:
+    print("z lebih besar")
+else:
+    print("tidak ada yang bear")    
