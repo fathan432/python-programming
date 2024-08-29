30ebcbf6354f09de38be6e54cbedbd0c24b83648
fathan432 <alfatharfathan@gmail.com>
2024-08-28 22:32:23 -0700

    Add files via upload

---
+++ tkj fathan/ujian.py
@@ -0,0 +1,26 @@
+nilai_tugas1 = float(input("Masukkan nilai tugas 1:"))
+nilai_tugas2 = float(input("Masukan nilai tugas 2:"))
+nilai_tugas3 = float(input("Masukan nilai tugas 3:"))
+
+# Menghitung rata-rata
+rata_rata = (nilai_tugas1 + nilai_tugas2 + nilai_tugas3) / 3
+
+# Menampilkan hasil
+print("Rata-rata nilai tugas adalah:", rata_rata)
+
+if rata_rata <= 70:
+    print("Tidak Lulus")
+
+elif rata_rata > 80:
+    print("Lulus")
+
+elif rata_rata > 50 and rata_rata< 69:
+    print("Perbaiki lagi")
+
+# Jumlah siswa yang akan dimasukkan
+    jumlah_siswa = int(input("Masukkan jumlah siswa: "))
+
+ # Loop untuk setiap siswa 
+for i in range(jumlah_siswa):nilai_tugas1 = float(input("Masukkan nilai tugas 1:"))
+nilai_tugas2 = float(input("Masukan nilai tugas 2:"))
+nilai_tugas3 = float(input("Masukan nilai tugas 3:"))
